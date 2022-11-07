0193ae66bfa60a119171d1712d8ec935ed92bd72
Alejandro Diaz Alvarado <alexndro7145@gmail.com>
2022-11-07 16:03:10 -0600

    views part 2

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def bienvenido(request):
     return HttpResponse('Hola mundo desde Django')
+
+def despedirse(request):
+    return HttpResponse('Despedida desde Django')
+
+def contacto(request):
+    return HttpResponse('email: ing.diaz.alvarado@gmail.com <br> teléfono: 5579332570')
